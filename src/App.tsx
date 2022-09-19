60200d58c1bbd4b8d9d77cbab0aac36b99fcf6c3
DaoZhang1588 <1205308740@qq.com>
2022-09-19 10:57:40 +0800

    重构

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { defineComponent, ref } from "vue";
+import {RouterView } from "vue-router";
+
+export const App=defineComponent({
+    setup(){ 
+        return()=><>
+        <header>导航</header>
+        <ul>
+            <li>
+                <router-link to="/">Foo</router-link>
+            </li>
+            <li>
+            <router-link to="/about">Bar</router-link>
+            </li>
+        </ul>
+        <div>
+            <RouterView/>
+        </div>
+        <footer>页脚</footer>
+        </>
+    }
+})
---
+++ src/config/routes.tsx
@@ -0,0 +1,8 @@
+import { Bar } from "../views/Bar";
+import { Foo } from "../views/Foo";
+
+
+export const routes =[  //路由规则
+    {path:'/',component:Foo},
+    {path:'/about',component:Bar}
+]
